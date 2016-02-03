3ed9bfedd1e75629a7f24832a7748400c07cabcb
Felipe Vasconcelos <fav@cesar.org.br>
2016-02-03 18:57:10 -0300

    Adding files

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var webserver = require('gulp-webserver');
+
+gulp.task('sass', function() {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function() {
+    return gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('webserver', function() {
+    return gulp.src('.')
+        .pipe(webserver({
+            host: '172.24.6.145',
+            port: '8080',
+            livereload: true,
+            directoryListing: false
+        }));
+});
+gulp.task('default', ['sass', 'sass:watch', 'webserver']);
